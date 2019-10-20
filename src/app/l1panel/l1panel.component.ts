ebc2bf5bae202221fc47607a98602db97b8c7792
aman <amanshah101997@gmail.com>
2019-10-20 23:01:36 +0530

    l1-panel change

--- src/app/l1panel/l1panel.component.ts
+++ src/app/l1panel/l1panel.component.ts
@@ -25,7 +25,7 @@ export class L1PanelComponent implements OnInit {
   ngOnInit() {
 
   }
-  addL1Panel(content) {
+  addL1Panel () {
     const modalRef = this.modalService.open(L1PanelCreateComponent, { size: 'lg', backdrop: 'static', keyboard: false });
     modalRef.result.then((data: Array<L1Panel>) => {
       if (!this.coreHelperService.isArrayEmpty(data)) {
@@ -34,4 +34,3 @@ export class L1PanelComponent implements OnInit {
     });
   }
 }
-
